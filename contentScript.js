b78e476e91dd566b6fe97fd5eddc54953103c276
myName <email>
2021-05-22 13:10:34 +0200

    ready to deploy

--- contentScript.js
+++ contentScript.js
@@ -1,27 +1,27 @@
 
 function loop(){
-	try{
-		// skipe video ad 
-		var collection = document.getElementsByClassName("ytp-ad-skip-button ytp-button");
-		if(collection != null){
-			var btn =    collection.item(0);
-			if(btn != null){
-				btn.click();
+	while(true){
+		try{
+			// skipe video ad 
+			var collection = document.getElementsByClassName("ytp-ad-skip-button ytp-button");
+			if(collection != null){
+				var btn =    collection.item(0);
+				if(btn != null){
+					btn.click();
+				}
 			}
-		}
-		
-		// close video banner 
-		var collection = document.getElementsByClassName("ytp-ad-overlay-close-button");
-		if(collection != null){
-			var btn =    collection.item(0);
-			if(btn != null){
-				btn.click();
+			
+			// close video banner 
+			var collection = document.getElementsByClassName("ytp-ad-overlay-close-button");
+			if(collection != null){
+				var btn =    collection.item(0);
+				if(btn != null){
+					btn.click();
+				}
 			}
+		}catch(e){
+			console.log(e);
 		}
-		
-		setTimeout(loop, 2000);
-	}catch(e){
-		console.log(e);
 	}
 }
 
