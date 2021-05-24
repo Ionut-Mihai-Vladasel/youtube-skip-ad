363cf5ab1cdd1b1e59e4d8ba0abf11cb128d4ad8
myName <email>
2021-05-24 19:28:05 +0200

    refactoring 2

--- contentScript.js
+++ contentScript.js
@@ -1,37 +1,58 @@
 
-function loop(){
-	
-		try{
-			// skipe video ad 
-			var collection = document.getElementsByClassName("ytp-ad-skip-button ytp-button");
-			if(collection != null){
-				var btn =    collection.item(0);
-				if(btn != null){
-					btn.click();
-				}
-			}
-			
-			// close video banner 
-			var collection = document.getElementsByClassName("ytp-ad-overlay-close-button");
-			if(collection != null){
-				var btn =    collection.item(0);
-				if(btn != null){
-					btn.click();
-				}
-			}
-			loop();
-		}catch(e){
-			console.log(e);
-		}
-	
-}
 
-window.addEventListener('DOMContentLoaded', (event) => {
+function findAndSkipAd(){
 	try{
-		loop();
+		// video without button 
+		let videoCollection = document.getElementsByClassName("video-stream html5-main-video");
+		if(videoCollection && videoCollection.item(0)){
+			var videoTag =  videoCollection.item(0);
+			
+			// btn info video can not be skiped 
+			var collection = document.getElementsByClassName("ytp-ad-text ytp-ad-preview-text");
+			if(collection && collection.item(0)){
+				// currentTime in seconds
+				videoTag.currentTime = videoTag.duration;
+				console.log("video cannot be skiped, just skiped");
+			}
+		}
+		
+		// video with skip btn 
+		var videoAdBtns = document.getElementsByClassName("ytp-ad-skip-button ytp-button");
+		if(videoAdBtns.length > 0){
+			for(let btn of videoAdBtns){
+					btn.click();
+					console.log("video skiped");
+			}
+		}
+		
+		// banner 
+		var overlayAdBtns = document.getElementsByClassName("ytp-ad-overlay-close-button");
+		if(overlayAdBtns.length > 0){
+			for(let btn of overlayAdBtns){
+					btn.click();
+					console.log("Banner closed");
+			}
+		}
 	}catch(e){
 		console.log(e);
 	}
+}
+
+function setup(){
+	let collection = document.getElementsByClassName("video-stream html5-main-video");
+	if(collection && collection.item(0)){
+		var videoTag = collection.item(0);
+			videoTag.addEventListener('canplay', (event) => {
+				 setTimeout(findAndSkipAd, 1000); 
+				 setTimeout(findAndSkipAd, 2000); 			
+				}); 
+	}
+	console.log("setup done");
+}
+ 
+
+window.addEventListener('DOMContentLoaded', (event) => { 
+	setup();
 })
 
-  
+setup();
